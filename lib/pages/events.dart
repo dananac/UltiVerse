da4369e8b35c89e21d0c147edfc562c433bf83cf
Jordan Rood <jrood2024@nevada.unr.edu>
2023-05-11 02:05:56 -0700

    Events & home page updates

--- lib/pages/events.dart
+++ lib/pages/events.dart
@@ -5,6 +5,101 @@ class EventsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-    body: Center(child: Text('Events', style: TextStyle(fontSize: 72, color: Colors.red.shade400),)),
+    //body: Center(child: Text('Events', style: TextStyle(fontSize: 72, color: Colors.red.shade400),)),
+      body: ListView(
+      children: <Widget>[
+        //Card(child: ListTile(title: Text('One-line ListTile'))),
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.calendar_month),
+            title: Text('Stanford Invite', style: TextStyle(color: Colors.red[400]),),
+            subtitle:
+                Text('Stevinsion, CA | March 15th-16th, 2024\n5 teams registered'),
+            trailing: Icon(Icons.more_vert),
+            isThreeLine: true,
+          ),
+        ),
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.calendar_month),
+            title: Text('Davis Ultimate Invitational', style: TextStyle(color: Colors.red[400]),),
+            subtitle:
+                Text('Davis, CA | October 18th-19th, 2024\n8 teams registered'),
+            trailing: Icon(Icons.more_vert),
+            isThreeLine: true,
+          ),
+        ),
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.calendar_month),
+            title: Text('NorCal DI Sectionals', style: TextStyle(color: Colors.red[400]),),
+            subtitle:
+                Text('Santa Cruz, CA | November 25th-26th, 2024\n10 teams registered'),
+            trailing: Icon(Icons.more_vert),
+            isThreeLine: true,
+          ),
+        ),
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.calendar_month),
+            title: Text('NorCal DI Regionals', style: TextStyle(color: Colors.red[400]),),
+            subtitle:
+                Text('Santa Barbara, CA | April 15th-16th, 2024\n16 teams registered'),
+            trailing: Icon(Icons.more_vert),
+            isThreeLine: true,
+          ),
+        ),
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.calendar_month),
+            title: Text('Boise BigSky Brawl', style: TextStyle(color: Colors.red[400]),),
+            subtitle:
+                Text('Boise, ID | June 3rd-4th, 2024\n12 teams registered'),
+            trailing: Icon(Icons.more_vert),
+            isThreeLine: true,
+          ),
+        ),
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.calendar_month),
+            title: Text('Chico State Invite', style: TextStyle(color: Colors.red[400]),),
+            subtitle:
+                Text('Chico, CA | March 15th-16th, 2024\n5 teams registered'),
+            trailing: Icon(Icons.more_vert),
+            isThreeLine: true,
+          ),
+        ),
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.calendar_month),
+            title: Text('BNP Paribas Open', style: TextStyle(color: Colors.red[400]),),
+            subtitle:
+                Text('Indian Wells, CA | March 15th-16th, 2024\n10 teams registered'),
+            trailing: Icon(Icons.more_vert),
+            isThreeLine: true,
+          ),
+        ),
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.calendar_month),
+            title: Text('The Wimbledon', style: TextStyle(color: Colors.red[400]),),
+            subtitle:
+                Text('London, England | March 15th-16th, 2024\n3 teams registered'),
+            trailing: Icon(Icons.more_vert),
+            isThreeLine: true,
+          ),
+        ),
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.calendar_month),
+            title: Text('Austin Rodeo Rumble Invite', style: TextStyle(color: Colors.red[400]),),
+            subtitle:
+                Text('Austin, TX | July 12th-17th, 2024\n20 teams registered'),
+            trailing: Icon(Icons.more_vert),
+            isThreeLine: true,
+          ),
+        ),
+      ],
+    ),
   );
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:ultiverse/main.dart';
+import 'package:ultiverse/pages/events.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-    body: Center(child: Text('Home', style: TextStyle(fontSize: 72, color: Colors.red.shade400),)),
-  );
-}
+  Widget build(BuildContext context) {
+    //body: Center(child: Text('Home', style: TextStyle(fontSize: 72, color: Colors.red.shade400),)),
+    //body: const ElevatedButtonExample()
+    final ButtonStyle style = ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
+
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Text('What would you like to do?', 
+            style: TextStyle(
+              fontSize: 24, 
+              fontWeight:FontWeight.w400,
+              color: Colors.red[400]
+            ),
+          ),
+          ElevatedButton(
+            style: style,
+            onPressed: () {},
+            child: const Text('Create a New Event'),
+          ),
+          const SizedBox(height: 30),
+          ElevatedButton(
+            style: style,
+            onPressed: () {
+              Navigator.push(
+                context, MaterialPageRoute(builder: (context) =>
+                const EventsPage())
+              );
+            },
+            child: const Text('Join an Event'),
+          ),
+        ],
+      ),
+    );
+  }
+} 

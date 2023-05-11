bd307be7254c3993999531d13ebe6348461b9808
dana <80922081+dananac@users.noreply.github.com>
2023-05-11 11:25:59 -0700

    brackets page updated

--- lib/pages/brackets.dart
+++ lib/pages/brackets.dart
@@ -5,6 +5,72 @@ class BracketsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-    body: Center(child: Text('Brackets', style: TextStyle(fontSize: 72, color: Colors.red.shade400),)),
-  );
+    //body: Center(child: Text('Brackets', style: TextStyle(fontSize: 72, color: Colors.red.shade400),)),
+   body: ListView(
+      children: <Widget>[
+        Column(
+          children: [
+            Text('Round 1', style: Theme.of(context).textTheme.headline6),
+            SizedBox(height: 16.0),
+            // First Game
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Text('UNR', style: TextStyle(color: Colors.red[400]),),
+                        SizedBox(height: 8.0),
+                        Text('vs', style: TextStyle(color: Colors.red[400]),),
+                        SizedBox(height: 8.0),
+                        Text('UCSC', style: TextStyle(color: Colors.red[400]),),
+                      ],
+                    ),
+                  ),
+                ),
+                // Second Game
+                Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Text('Chico State', style: TextStyle(color: Colors.red[400]),),
+                        SizedBox(height: 8.0),
+                        Text('vs', style: TextStyle(color: Colors.red[400]),),
+                        SizedBox(height: 8.0),
+                        Text('UCSB', style: TextStyle(color: Colors.red[400]),),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 16.0),
+            // Third Game
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Text('UNR', style: TextStyle(color: Colors.red[400]),),
+                        SizedBox(height: 8.0),
+                        Text('vs', style: TextStyle(color: Colors.red[400]),),
+                        SizedBox(height: 8.0),
+                        Text('UCSB', style: TextStyle(color: Colors.red[400]),),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            ],
+          ),
+       ]
+    )
+  )
 }

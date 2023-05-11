a5fdd1a688bf89c56ddeee43e83d6313f5ee3e4a
Jordan Rood <jrood2024@nevada.unr.edu>
2023-05-11 10:30:26 -0700

    Updates to home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:ultiverse/main.dart';
 import 'package:ultiverse/pages/events.dart';
 
 class HomePage extends StatelessWidget {
@@ -15,25 +14,56 @@ class HomePage extends StatelessWidget {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
-          Text('What would you like to do?', 
-            style: TextStyle(
-              fontSize: 24, 
-              fontWeight:FontWeight.w400,
-              color: Colors.red[400]
+          const Card(
+              child: Padding(
+              padding: EdgeInsets.all(32.0),
+              child: Text('Event Planning', style: TextStyle(fontSize: 44, fontWeight: FontWeight.bold),),
+            ),
+          ),
+          Image.asset('assets/images/ultimate_pic.jpg', height: 280,),
+          Card(
+            child: Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Text('What would you like to do?', 
+                style: TextStyle(
+                  fontSize: 24, 
+                  fontWeight:FontWeight.w400,
+                  color: Colors.red[400]
+                ),
+              ),
             ),
           ),
           ElevatedButton(
             style: style,
-            onPressed: () {},
-            child: const Text('Create a New Event'),
+            onPressed: () {
+              Navigator.push(
+                context, MaterialPageRoute(builder: (context) {
+                  return Scaffold(
+                    appBar: AppBar(
+                      title: Text('Event Planning - Step 1', style: TextStyle(color: Colors.red[400]),),
+                    ),
+                    body: Center(
+                      child: Text('This will be our step 1 screen to create a new event!', style: TextStyle(fontSize: 44, fontWeight: FontWeight.bold),)
+                    )
+                  );
+                })
+              );
+          },
+          child: const Text('Create a New Event'),
           ),
           const SizedBox(height: 30),
           ElevatedButton(
             style: style,
             onPressed: () {
               Navigator.push(
-                context, MaterialPageRoute(builder: (context) =>
-                const EventsPage())
+                context, MaterialPageRoute(builder: (context) {
+                  return Scaffold(
+                    appBar: AppBar(
+                      title: Text('Events', style: TextStyle(color: Colors.red[400]),),
+                    ),
+                    body: EventsPage()
+                  );
+                })
               );
             },
             child: const Text('Join an Event'),

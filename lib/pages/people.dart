a3a8bcc1b0f46db3033ff9e3800bea2241d30be9
dana <80922081+dananac@users.noreply.github.com>
2023-05-11 10:57:48 -0700

    people page updated

--- lib/pages/people.dart
+++ lib/pages/people.dart
@@ -5,6 +5,81 @@ class PeoplePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-    body: Center(child: Text('People', style: TextStyle(fontSize: 72, color: Colors.red.shade400),)),
+    //body: Center(child: Text('Events', style: TextStyle(fontSize: 72, color: Colors.red.shade400),)),
+    body: ListView(
+      children: <Widget>[
+        //Card(child: ListTile(title: Text('One-line ListTile'))),
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.calendar_month),
+            title: Text('Dana Conley', style: TextStyle(color: Colors.red[400]),),
+            subtitle:
+            Text('University of Nevada - Reno\ndanaconley@nevada.unr.edu'),
+            trailing: Icon(Icons.more_vert),
+            isThreeLine: true,
+          ),
+        ),
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.calendar_month),
+            title: Text('Jordan Rood', style: TextStyle(color: Colors.red[400]),),
+            subtitle:
+            Text('University of Nevada - Reno\njrood2024@nevada.unr.edu'),
+            trailing: Icon(Icons.more_vert),
+            isThreeLine: true,
+          ),
+        ),
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.calendar_month),
+            title: Text('Jenna Young', style: TextStyle(color: Colors.red[400]),),
+            subtitle:
+            Text('UC Santa Cruz\njyoung@ucsc.edu'),
+            trailing: Icon(Icons.more_vert),
+            isThreeLine: true,
+          ),
+        ),
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.calendar_month),
+            title: Text('Alexis Freeman', style: TextStyle(color: Colors.red[400]),),
+            subtitle:
+            Text('UC Santa Barbara\nalexisfree@ucsb.edu'),
+            trailing: Icon(Icons.more_vert),
+            isThreeLine: true,
+          ),
+        ),
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.calendar_month),
+            title: Text('Hannah James', style: TextStyle(color: Colors.red[400]),),
+            subtitle:
+            Text('Boise State university\nhannahj@bsu.edu'),
+            trailing: Icon(Icons.more_vert),
+            isThreeLine: true,
+          ),
+        ),
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.calendar_month),
+            title: Text('McKenna Coleman', style: TextStyle(color: Colors.red[400]),),
+            subtitle:
+            Text('Chico State\nmccole@gmail.com'),
+            trailing: Icon(Icons.more_vert),
+            isThreeLine: true,
+          ),
+        ),
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.calendar_month),
+            title: Text('Rachael Johnson', style: TextStyle(color: Colors.red[400]),),
+            subtitle:
+            Text('UC Santa Cruz\nrachaeljohnson@ucsc.edu'),
+            trailing: Icon(Icons.more_vert),
+            isThreeLine: true,
+          ),
+        ),
+      ],
+    ),
   );
 }
